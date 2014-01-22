fa6ce704ece566376e378d8127fcb7b1f3f22caf
benjamin.pierrot@univ-st-etienne.fr <benjamin.pierrot@univ-st-etienne.fr>
2014-01-22 22:47:12 +0100

    Ajout affichage categories

--- src/Nfe102/Bundle/RestoBundle/Controller/ProduitController.php
+++ src/Nfe102/Bundle/RestoBundle/Controller/ProduitController.php
@@ -4,52 +4,49 @@ namespace Nfe102\Bundle\RestoBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use Nfe102\Bundle\RestoBundle\Entity\Produit;
 use Nfe102\Bundle\RestoBundle\Entity\CategorieProduit;
 use Nfe102\Bundle\RestoBundle\Form\ProduitType;
 use Nfe102\Bundle\RestoBundle\Form\CategorieProduitType;
-
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
 use Symfony\Component\HttpFoundation\Response;
+
 /**
  * Produit controller.
  *
  */
-class ProduitController extends Controller
-{
+class ProduitController extends Controller {
 
     /**
      * Lists all Produit entities.
      *
      */
-    public function indexAction()
-    {
+    public function indexAction() {
         $em = $this->getDoctrine()->getManager();
 
         $entities = $em->getRepository('Nfe102RestoBundle:Produit')->findAll();
 
         return $this->render('Nfe102RestoBundle:Produit:index.html.twig', array(
-            'entities' => $entities,
-        ));
+                    'entities' => $entities,
+                ));
     }
-    
+
     /**
      * Lists json product
      */
     public function jsonshowAction() {
-        
-        
-    $em = $this->getDoctrine()->getManager();
-    $query = $em->createQuery(
-    'SELECT p.id,p.nom,p.description,p.image,p.prix
+
+
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+     'SELECT p.id,p.nom,p.description,p.image,p.prix
      FROM Nfe102RestoBundle:Produit p
      WHERE p.dispo LIKE :dispo
      ORDER BY p.prix ASC'
-     )->setParameter('dispo', '1111111');
+                )->setParameter('dispo', '1111111');
 
         $entities = $query->getResult();
 
@@ -59,21 +56,18 @@ class ProduitController extends Controller
         $json = $serializer->serialize($entities, 'json');
 
         return new Response($json, 200, array('Content-Type' => 'application/json'));
-  
     }
-
 
     /**
      * Creates a new Produit entity.
      *
      */
-    public function createAction(Request $request)
-    {
+    public function createAction(Request $request) {
         $entity = new Produit();
-        
-        $date = new \DateTime("now");     
+
+        $date = new \DateTime("now");
         $entity->setDatecreateprod($date);
-        
+
         $form = $this->createCreateForm($entity);
         $form->handleRequest($request);
 
@@ -86,24 +80,23 @@ class ProduitController extends Controller
         }
 
         return $this->render('Nfe102RestoBundle:Produit:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-        ));
+                    'entity' => $entity,
+                    'form' => $form->createView(),
+                ));
     }
 
     /**
-    * Creates a form to create a Produit entity.
-    *
-    * @param Produit $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createCreateForm(Produit $entity)
-    {
+     * Creates a form to create a Produit entity.
+     *
+     * @param Produit $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Produit $entity) {
         $form = $this->createForm(new ProduitType(), $entity, array(
             'action' => $this->generateUrl('produit_create'),
             'method' => 'POST',
-        ));
+                ));
 
         $form->add('submit', 'submit', array('label' => 'Create'));
 
@@ -114,27 +107,25 @@ class ProduitController extends Controller
      * Displays a form to create a new Produit entity.
      *
      */
-    public function newAction()
-    {
+    public function newAction() {
         $entity = new Produit();
-        $form   = $this->createCreateForm($entity);
-        
-        
+        $form = $this->createCreateForm($entity);
+
+
         $CategorieProduitType = $this->createForm(new CategorieProduitType());
-        
+
         return $this->render('Nfe102RestoBundle:Produit:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
-            'CategorieProduitType' => $CategorieProduitType->createView()
-        ));
+                    'entity' => $entity,
+                    'form' => $form->createView(),
+                    'CategorieProduitType' => $CategorieProduitType->createView()
+                ));
     }
 
     /**
      * Finds and displays a Produit entity.
      *
      */
-    public function showAction($id)
-    {
+    public function showAction($id) {
         $em = $this->getDoctrine()->getManager();
 
         $entity = $em->getRepository('Nfe102RestoBundle:Produit')->find($id);
@@ -146,45 +137,58 @@ class ProduitController extends Controller
         $deleteForm = $this->createDeleteForm($id);
 
         return $this->render('Nfe102RestoBundle:Produit:show.html.twig', array(
-            'entity'      => $entity,
-            'delete_form' => $deleteForm->createView(),        ));
+                    'entity' => $entity,
+                    'delete_form' => $deleteForm->createView(),));
     }
-    
-        /**
+
+    /**
+     * Finds and displays a Produit categorie.
+     *
+     */
+    public function showcatAction(Request $request) {
+       
+        $idprod = $request->get('idprod');
+        
+        if (isset($idprod)) {
+        
+        $product = $this->getDoctrine()
+        ->getRepository('Nfe102RestoBundle:Produit')
+        ->find($idprod);
+ 
+        $cat = $product->getIdcategorie();
+        
+        return new Response($cat[0]->getType());
+       } else {
+        return new Response('null');  
+       }
+    }
+
+    /**
      * Finds and displays a Produit entity (view only).
      *
      */
-    public function showviewAction(Request $request)
-    {
+    public function showviewAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
 
         $entity = $em->getRepository('Nfe102RestoBundle:Produit')->find($request->get('id'));
-         //   $entity2 = $em->getRepository('Nfe102RestoBundle:CategorieProduit')->findOneBytype('PIZZA');
-            
+
         if (!$entity) {
             throw $this->createNotFoundException('Unable to find Produit entity.');
         }
-//        var_dump($entity);die();
-//        $serializer = new Serializer(array(new GetSetMethodNormalizer()), array('json' => new
-//                    JsonEncoder()));
-//        $json = $serializer->serialize($entity, 'json');
-        //echo $entity->getIdcategorie(); //, 'idCat' =>$entity2->getId()
-        
-            $entie = array('id' => $entity->getId(),'nom'=>$entity->getnom()
-                    ,'prix'=>$entity->getprix(),'description'=>$entity->getdescription());
-      
-        
+
+
+        $entie = array('id' => $entity->getId(), 'nom' => $entity->getnom()
+            , 'prix' => $entity->getprix(), 'description' => $entity->getdescription());
+
+
         return new Response(json_encode($entie), 200, array('Content-Type' => 'application/json'));
-  
-       
     }
 
     /**
      * Displays a form to edit an existing Produit entity.
      *
      */
-    public function editAction($id)
-    {
+    public function editAction($id) {
         $em = $this->getDoctrine()->getManager();
 
         $entity = $em->getRepository('Nfe102RestoBundle:Produit')->find($id);
@@ -197,41 +201,40 @@ class ProduitController extends Controller
         $deleteForm = $this->createDeleteForm($id);
 
         return $this->render('Nfe102RestoBundle:Produit:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
+                    'entity' => $entity,
+                    'edit_form' => $editForm->createView(),
+                    'delete_form' => $deleteForm->createView(),
+                ));
     }
 
     /**
-    * Creates a form to edit a Produit entity.
-    *
-    * @param Produit $entity The entity
-    *
-    * @return \Symfony\Component\Form\Form The form
-    */
-    private function createEditForm(Produit $entity)
-    {
+     * Creates a form to edit a Produit entity.
+     *
+     * @param Produit $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createEditForm(Produit $entity) {
         $form = $this->createForm(new ProduitType(), $entity, array(
             'action' => $this->generateUrl('produit_update', array('id' => $entity->getId())),
             'method' => 'PUT',
-        ));
+                ));
 
         $form->add('submit', 'submit', array('label' => 'Update'));
 
         return $form;
     }
+
     /**
      * Edits an existing Produit entity.
      *
      */
-    public function updateAction(Request $request, $id)
-    {
+    public function updateAction(Request $request, $id) {
         $em = $this->getDoctrine()->getManager();
 
         $entity = $em->getRepository('Nfe102RestoBundle:Produit')->find($id);
-        
-        $date = new \DateTime("now");     
+
+        $date = new \DateTime("now");
         $entity->setDatecreateprod($date);
 
         if (!$entity) {
@@ -249,24 +252,24 @@ class ProduitController extends Controller
         }
 
         return $this->render('Nfe102RestoBundle:Produit:edit.html.twig', array(
-            'entity'      => $entity,
-            'edit_form'   => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
-        ));
+                    'entity' => $entity,
+                    'edit_form' => $editForm->createView(),
+                    'delete_form' => $deleteForm->createView(),
+                ));
     }
+
     /**
      * Deletes a Produit entity.
      *
      */
-    public function deleteAction(Request $request, $id)
-    {
+    public function deleteAction(Request $request, $id) {
         $form = $this->createDeleteForm($id);
         $form->handleRequest($request);
 
         if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
             $entity = $em->getRepository('Nfe102RestoBundle:Produit')->find($id);
-            
+
             if (!$entity) {
                 throw $this->createNotFoundException('Unable to find Produit entity.');
             }
@@ -285,13 +288,13 @@ class ProduitController extends Controller
      *
      * @return \Symfony\Component\Form\Form The form
      */
-    private function createDeleteForm($id)
-    {
+    private function createDeleteForm($id) {
         return $this->createFormBuilder()
-            ->setAction($this->generateUrl('produit_delete', array('id' => $id)))
-            ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
-            ->getForm()
+                        ->setAction($this->generateUrl('produit_delete', array('id' => $id)))
+                        ->setMethod('DELETE')
+                        ->add('submit', 'submit', array('label' => 'Delete'))
+                        ->getForm()
         ;
     }
+
 }
